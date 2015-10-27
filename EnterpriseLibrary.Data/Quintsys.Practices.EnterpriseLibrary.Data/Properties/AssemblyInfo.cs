c8a9f3c778e6d5271a4060f2bbffc9565a9dce95
Erich Quintero <qbantek@gmail.com>
2015-10-26 20:56:04 -0400

    latest nuget paks

--- EnterpriseLibrary.Data/Quintsys.Practices.EnterpriseLibrary.Data/Properties/AssemblyInfo.cs
+++ EnterpriseLibrary.Data/Quintsys.Practices.EnterpriseLibrary.Data/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Quintsys.Practices.EnterpriseLibrary.Data")]
-[assembly: AssemblyDescription("The Data Access Application Block abstracts the actual database you are using, and exposes a collection of methods that make it easy to access that database and to perform common tasks. The block is designed to simplify the task of calling stored procedures, but it also provides full support for the use of parameterized SQL statements.")]
+[assembly: AssemblyDescription("Contains extensions and custom classes for the Microsoft.Practices.EnterpriseLibrary.Data namespace.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Quintsys.Practices.EnterpriseLibrary.Data")]
@@ -11,4 +11,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2ad1faf2-3c1a-4cb7-b9c5-2cf8f0958cbc")]
-[assembly: AssemblyVersion("1.0.3")]
+[assembly: AssemblyVersion("1.0.4")]
